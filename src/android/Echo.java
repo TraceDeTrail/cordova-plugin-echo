1e80d419490fc840d0ad323ce05e275ace27439b
TraceDeTrail <admin@tracedetrail.fr>
2017-10-02 11:33:30 +0200

    test12

--- src/android/Echo.java
+++ src/android/Echo.java
@@ -7,26 +7,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
-* This class echoes a string called from JavaScript.
-*/
+
 public class Echo extends CordovaPlugin {
 
-@Override
-public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("echo")) {
-        String message = args.getString(0);
-        this.echo(message, callbackContext);
-        return true;
-    }
-    return false;
-}
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+      if (action.equals("echo")) {
+          callbackContext.success("yes");
+          return true;
+      }
+      return false;
+  }
 
-private void echo(String message, CallbackContext callbackContext) {
-    if (message != null && message.length() > 0) {
-        callbackContext.success(message);
-    } else {
-        callbackContext.error("Expected one non-empty string argument.");
-    }
-}
 }
