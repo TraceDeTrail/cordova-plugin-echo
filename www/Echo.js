05c39ebef575c13ce5b1806259b54c1f0fa8cea2
TraceDeTrail <admin@tracedetrail.fr>
2017-10-02 06:19:10 +0200

    test5

--- www/Echo.js
+++ www/Echo.js
@@ -1,5 +1,14 @@
-window.echo = function(str, callback) {
+
+var Echo = function () {
+    this.name = "Echo";
+};
+
+Echo.prototype.echo = function(str, callback) {
     cordova.exec(callback, function(err) {
         callback('Nothing to echo.');
     }, "Echo", "echo", [str]);
 };
+
+
+
+module.exports = new Echo();
