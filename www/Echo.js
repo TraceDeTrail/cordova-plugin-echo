44fa186369a0b907d834342d03a10d5652a91656
TraceDeTrail <admin@tracedetrail.fr>
2017-10-02 07:17:35 +0200

    test5

--- www/Echo.js
+++ www/Echo.js
@@ -4,9 +4,10 @@ var Echo = function () {
 };
 
 Echo.prototype.echo = function(str, callback) {
-    cordova.exec(callback, function(err) {
-        callback('Nothing to echo.');
-    }, "Echo", "echo", [str]);
+    // cordova.exec(callback, function(err) {
+    //     callback('Nothing to echo.');
+    // }, "Echo", "echo", [str]);
+    alert('test');
 };
 
 
